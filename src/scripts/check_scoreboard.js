0f05915b6f680eed3b9d41ecb0582d4d73c4e2c1
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-01-09 19:19:57 -0500

    Fix leaderboard check functionality

--- src/scripts/check_scoreboard.js
+++ src/scripts/check_scoreboard.js
@@ -1,6 +1,6 @@
 function checkScores (leaderScores, score) {
     const scores = Object.values(leaderScores)
-    return scores.every(entry => entry.score > score)
+    return scores.every(entry => entry.score >= score)
 }
 
 export default checkScores;
